2176fb1e6a8bb546a8e1ecb175fc740685ec25f8
SamuelLFA <samuellfa3@gmail.com>
2025-06-14 11:28:58 -0300

    test: add test reader for not found file

--- reader_test.go
+++ reader_test.go
@@ -10,12 +10,17 @@ import (
 const _validfile = "test_files/valid_file.xlsx"
 const _hugeworksheetname = "test_files/huge_worksheet_name.xlsx"
 
-func TestNewReader(t *testing.T) {
+func TestNewReader_ValidFile(t *testing.T) {
 	r, err := tabeludo.NewReader(_validfile)
 	assert.Nil(t, err)
 
 	assert.NotNil(t, r)
 	r.Close()
+}
+
+func TestNewReader_NotFound(t *testing.T) {
+	_, err := tabeludo.NewReader("")
+	assert.Equal(t, "opening XLSX file '': open : no such file or directory", err.Error())
 }
 
 func TestClose(t *testing.T) {
